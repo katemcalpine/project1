b66777f42469922785a6e739e2f9eaebe2fe0ffa
Kate McAlpine <kate.mcalpine@uon.edu.au>
2019-04-26 13:16:09 +0000

    updated code

--- mainCode.c
+++ mainCode.c
@@ -11,11 +11,11 @@
 #include <string.h>
 
 void rotDecCip(char cipher[]);
-void rotDecMsgKey(char message[], char key[]);
+void rotDecCipKey(char message[], char key[]);
 void rotEncMsgKey(char message[], char key[]);
 void subDecCip(char cipher[]);
-void subDecMsgKey(char message[], char key[], const char alphabetEng[]);
-void subEncMsgKey(char message[], char key[]); const char alphabetEng[]);
+void subDecCipKey(char message[], char key[], const char alphabetEng[]);
+void subEncMsgKey(char message[], char key[], const char alphabetEng[]);
 
 int main()
 {
@@ -25,29 +25,106 @@ int main()
     char cipher[255];
     const char alphabetEng[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     scanf("%d", &input);
-    printf("Task selected: %d\n", input);
+    //printf("Task selected: %d\n", input);
     
     switch (input)
     {
         case 1:
-            rotDecCip();
+            printf("Rotation Decryption Using Only Cipher\n\n");
+            rotDecCip(cipher);
             break;
         case 2:
-            rotDecMsgKey();
+            printf("Rotation Decryption Using Cipher and Key\n\n");
+            rotDecCipKey(cipher, key);
             break;
         case 3:
-            rotEncMsgKey();
+            printf("Rotation Encryption Using Message and Key\n\n");
+            rotEncMsgKey(message, key);
             break;
         case 4:
-            subDecCip();
+            printf("Sustitution Decryption Using Only Cipher\n\n");
+            subDecCip(cipher);
             break;
         case 5:
-            subDecMsgKey();
+            printf("Substitution Decryption Using Cipher and Key\n\n");
+            subDecCipKey(cipher, key, alphabetEng);
             break;
         case 6:
-            subEncMsgKey();
+            printf("Substitution Encryption Using Message and Key\n\n");
+            subEncMsgKey(message, key, alphabetEng);
             break;
         default:
             printf("%d is not an option.\n", input);
+    }
+    return 0;
+}
+
+void rotDecCip(char cipher[])
+{
+    FILE *input;
+    input = fopen("rotation", "r");
+    if(input == NULL)
+    {
+        perror("fopen()");
+        return;
+    }
+    else
+    {
+        
+    }
+}
+
+void rotDecCipKey(char message[], char key[])
+{
+    FILE *input;
+    input = fopen("rotation", "r");
+    if(input == NULL)
+    {
+        perror("fopen()");
+        return;
+    }
+}
+
+void rotEncMsgKey(char message[], char key[])
+{
+    FILE *input;
+    input = fopen("rotation", "r");
+    if(input == NULL)
+    {
+        perror("fopen()");
+        return;
+    } 
+}
+
+void subDecCip(char cipher[])
+{
+    FILE *input;
+    input = fopen("substitution", "r");
+    if(input == NULL)
+    {
+        perror("fopen()");
+        return;
+    }
+}
+
+void subDecCipKey(char message[], char key[], const char alphabetEng[])
+{
+    FILE *input;
+    input = fopen("substitution", "r");
+    if(input == NULL)
+    {
+        perror("fopen()");
+        return;
+    }
+}
+
+void subEncMsgKey(char message[], char key[], const char alphabetEng[])
+{
+    FILE *input;
+    input = fopen("substitution", "r");
+    if(input == NULL)
+    {
+        perror("fopen()");
+        return;
     }
 }
