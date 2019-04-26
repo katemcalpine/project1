eab10a8f6b1d2debfeb4b23fbe3393a528aa2c0f
Kate McAlpine <kate.mcalpine@uon.edu.au>
2019-04-26 05:39:57 +0000

    changed files

--- mainCode.c
+++ mainCode.c
@@ -1,6 +1,24 @@
+/*
+ * Encryption/Decryption Program
+ * 
+ * Created on: 26/04/19
+ * Author: Kate McAlpine (c3078083)
+ * Class: ENGG1003
+ * Lab: EFG14
+ */
+
 #include <stdio.h>
+#include <string.h>
+
+void rotDecCip();
+void rotDecMsgKey();
+void rotEncMsgKey();
+void subDecCip();
+void subDecMsgKey();
+void subEncMsgKey();
 
 int main(void)
 {
+    const char alphabetEng[] = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     printf("hello, world\n");
 }
