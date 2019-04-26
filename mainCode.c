746a3eff73f50df9fb5cc2331155c9ba4b9db679
Kate McAlpine <kate.mcalpine@uon.edu.au>
2019-04-26 11:05:22 +0000

    updated code

--- mainCode.c
+++ mainCode.c
@@ -10,15 +10,44 @@
 #include <stdio.h>
 #include <string.h>
 
-void rotDecCip();
-void rotDecMsgKey();
-void rotEncMsgKey();
-void subDecCip();
-void subDecMsgKey();
-void subEncMsgKey();
+void rotDecCip(char cipher[]);
+void rotDecMsgKey(char message[], char key[]);
+void rotEncMsgKey(char message[], char key[]);
+void subDecCip(char cipher[]);
+void subDecMsgKey(char message[], char key[], const char alphabetEng[]);
+void subEncMsgKey(char message[], char key[]); const char alphabetEng[]);
 
-int main(void)
+int main()
 {
-    const char alphabetEng[] = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    printf("hello, world\n");
+    int input;
+    char message[255];
+    char key[26];
+    char cipher[255];
+    const char alphabetEng[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    scanf("%d", &input);
+    printf("Task selected: %d\n", input);
+    
+    switch (input)
+    {
+        case 1:
+            rotDecCip();
+            break;
+        case 2:
+            rotDecMsgKey();
+            break;
+        case 3:
+            rotEncMsgKey();
+            break;
+        case 4:
+            subDecCip();
+            break;
+        case 5:
+            subDecMsgKey();
+            break;
+        case 6:
+            subEncMsgKey();
+            break;
+        default:
+            printf("%d is not an option.\n", input);
+    }
 }
